af3a1ae7950f28379f1142a5f1edf0ff4cfc3eb9
zengzhe <Zeng_Zhe@outlook.com>
2024-03-26 11:23:36 +0800

    feat: update 文案

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: zengzhe
+ * @Date: 2024-03-01 11:34:10
+ * @LastEditors: zengzhe
+ * @LastEditTime: 2024-03-26 11:22:45
+ * @Description: 
+ */
 /* eslint-disable @next/next/no-page-custom-font */
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
@@ -10,8 +17,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "哲的AI Chat",
+  description: "打造自己的人工智能助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +29,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "哲的AI Chat",
     statusBarStyle: "default",
   },
 };
